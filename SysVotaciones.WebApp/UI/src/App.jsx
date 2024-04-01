0f7e26c896e87973e99175a7787486c5677e6f9c
edgar sibrian <edg4r168@gmail.com>
2024-04-01 14:48:48 -0600

    add  new routes

--- SysVotaciones.WebApp/UI/src/App.jsx
+++ SysVotaciones.WebApp/UI/src/App.jsx
@@ -1,20 +1,42 @@
 import "./App.css";
+import "src/Components/Notification/index.css";
+
 import { LoginPageAdmin } from "./Pages/Admin/Login";
 import { RegisterPage } from "./Pages/User/Register";
 import { LoginPageUser } from "./Pages/User/Login";
-import { Route } from "wouter";
+import { Route, Router } from "wouter";
 import { Landing } from "./Pages/User/Landing/Landing";
+import { Home } from "./Pages/User/Home";
+import { UserProvider } from "./context/userContext";
+import { AdminProvider } from "./context/adminContext";
+// import { Table } from "./Components/Table/Table";
+import { TCategory } from "./Components/Admin/TCategory/TCategory";
+import { Dashboard } from "./Components/Admin/Dashboard/Dashboard";
+import { Modal } from "./Components/Modal/Modal";
 
 function App() {
   return (
     <>
-      {/* User routes */}
-      <Route path="/" component={Landing} />
-      <Route path="/register" component={RegisterPage} />
-      <Route path="/login-user" component={LoginPageUser} />
+      <UserProvider>
+        {/* User routes */}
+        <Route path="/" component={Landing} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/login-user" component={LoginPageUser} />
+        <Route path="/home" component={Home} />
+      </UserProvider>
 
-      {/* Admin routes */}
-      <Route path="/login-admin" component={LoginPageAdmin} />
+      <Route path="/modal" component={Modal} />
+
+      <Route path="/table" component={TCategory} />
+
+      <AdminProvider>
+        {/* Admin routes */}
+        <Route path="/login-admin" component={LoginPageAdmin} />
+
+        <Router base="/dashboard">
+          <Route path="/:section?" component={Dashboard} />
+        </Router>
+      </AdminProvider>
     </>
   );
 }
