a8da41d3b828586dc7e3f710d890902decaf5a7d
edgar sibrian <edg4r168@gmail.com>
2024-04-01 15:03:25 -0600

    add component ListOfUsers

---
+++ SysVotaciones.WebApp/UI/src/Components/Admin/TUsers/ListOfUsers.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { EditIcon, TrashIcon } from "src/Components/Icons";
+import { ModalPortal } from "src/Components/Modal/Modal";
+import { FormEdit } from "./FormEdit";
+import { FormDelete } from "./FormDelete";
+import { TABLE_ACTIONS } from "src/consts/consts";
+import { useTable } from "src/hooks/useTable";
+
+export function ListOfUsers() {
+  const [showModal, setShowModal] = useState({ isOpen: false, action: null });
+  const { loading, entries, getEntry } = useTable();
+
+  const handleOnClose = () => {
+    setShowModal({ action: null, isOpen: false });
+  };
+
+  const handleOnEdit = (id) => {
+    setShowModal({ isOpen: true, action: TABLE_ACTIONS.edit });
+    getEntry(id);
+  };
+
+  const handleOnDelete = (id) => {
+    setShowModal({ isOpen: true, action: "delete" });
+    getEntry(id);
+  };
+
+  const buttonCursor = {
+    cursor: loading ? "progress" : "",
+  };
+
+  return (
+    <>
+      {entries.length !== 0 ? (
+        entries.map((entry) => {
+          if (entry == null) return;
+
+          return (
+            <tr key={entry.studentCode}>
+              <td>{entry.studentCode}</td>
+              <td>{entry.careerYear}</td>
+              <td>{entry.career}</td>
+              <td className="actions center">
+                <button
+                  style={buttonCursor}
+                  onClick={() => handleOnDelete(entry.studentCode)}
+                >
+                  <TrashIcon />
+                </button>
+
+                <button
+                  style={buttonCursor}
+                  onClick={() => handleOnEdit(entry.studentCode)}
+                >
+                  <EditIcon />
+                </button>
+              </td>
+            </tr>
+          );
+        })
+      ) : (
+        <tr>
+          <td></td>
+          <td>No se encontraron resultados</td>
+        </tr>
+      )}
+
+      <ModalPortal
+        onClose={handleOnClose}
+        openModal={
+          showModal.action === TABLE_ACTIONS.edit ? showModal.isOpen : false
+        }
+      >
+        <FormEdit onCancel={handleOnClose} onSubmit={handleOnClose} />
+      </ModalPortal>
+
+      <ModalPortal
+        onClose={handleOnClose}
+        openModal={
+          showModal.action === TABLE_ACTIONS.delete ? showModal.isOpen : false
+        }
+        height={200}
+        width={300}
+      >
+        <FormDelete onSubmit={handleOnClose} />
+      </ModalPortal>
+    </>
+  );
+}
