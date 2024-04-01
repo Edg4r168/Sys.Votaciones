969290f79b2e94f1c110ba8c3322603305af5fe8
edgar sibrian <edg4r168@gmail.com>
2024-04-01 14:53:49 -0600

    add component FormGroup

---
+++ SysVotaciones.WebApp/UI/src/Components/Admin/Forms/FormGroup.jsx
@@ -0,0 +1,7 @@
+export function FormGroup({ children }) {
+  return <div className="form-group">{children}</div>;
+}
+
+export function ButtonsGroup({ children }) {
+  return <div className="container-btns">{children}</div>;
+}
