c9b1f85f432e0c56a1e6368858493bcd5d031c08
edgar sibrian <edg4r168@gmail.com>
2024-04-01 14:52:56 -0600

    add components sidebar

---
+++ SysVotaciones.WebApp/UI/src/Components/Admin/Dashboard/SideBar.jsx
@@ -0,0 +1,34 @@
+import { UsersIcon } from "src/Components/Icons";
+import { Link } from "wouter";
+import { SECTIONS_DASHBOARD } from "../../../consts/sections";
+
+const ICONS = {
+  categories: <UsersIcon />,
+  users: <UsersIcon />,
+  participants: <UsersIcon />,
+};
+
+export function SideBar({ currentSection }) {
+  const isSelected = (section) =>
+    currentSection === section ? "isSelected" : "";
+
+  return (
+    <aside className="sidebar border">
+      <nav className="navigation">
+        {SECTIONS_DASHBOARD.map((setion) => {
+          const Icon = ICONS[setion.path];
+          return (
+            <Link
+              key={setion.title}
+              className={isSelected(setion.path)}
+              to={`/${setion.path}`}
+            >
+              {Icon}
+              {setion.title}
+            </Link>
+          );
+        })}
+      </nav>
+    </aside>
+  );
+}
