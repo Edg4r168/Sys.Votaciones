324118e91311472b50f519f29000be52d39bce8c
edgar sibrian <edg4r168@gmail.com>
2024-04-01 14:52:06 -0600

    add component dashboard

---
+++ SysVotaciones.WebApp/UI/src/Components/Admin/Dashboard/Dashboard.jsx
@@ -0,0 +1,39 @@
+import "./index.css";
+import { DropDownMenu } from "src/Components/DropDownMenu";
+import { TableProvider } from "src/context/tableContext";
+import { SideBar } from "./SideBar";
+import { Match } from "src/Components/Match/Match";
+import { SECTIONS_DASHBOARD } from "../../../consts/sections";
+import { TABLES } from "src/consts/consts";
+
+export function Dashboard({ params }) {
+  const currentSection = params?.section;
+
+  return (
+    <section className="dashboard">
+      <header className="header-dashboard">
+        <DropDownMenu />
+      </header>
+
+      <SideBar currentSection={currentSection} />
+
+      <main className="border">
+        {SECTIONS_DASHBOARD.map(({ path, Component }) => {
+          if (Component == null) return null;
+
+          return (
+            <Match
+              key={path}
+              path={path}
+              component={
+                <TableProvider table={TABLES[path]}>
+                  <Component />
+                </TableProvider>
+              }
+            />
+          );
+        })}
+      </main>
+    </section>
+  );
+}
