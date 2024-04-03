a830f3e988583965c0f6ce7f21f829fec80aeb47
edgar sibrian <edg4r168@gmail.com>
2024-04-03 14:32:16 -0600

    create and add sections

--- SysVotaciones.WebApp/UI/src/consts/sections.js
+++ SysVotaciones.WebApp/UI/src/consts/sections.js
@@ -1,6 +1,10 @@
+import { TParticipant } from "src/Components/Admin/TParticipant/TParticipant";
 import { TCareers } from "../Components/Admin/TCareers/TCareers";
 import { TCategory } from "../Components/Admin/TCategory/TCategory";
 import { TUsers } from "../Components/Admin/TUsers/TUsers";
+import { TTypeContest } from "src/Components/Admin/TTypeContest/TTypeContest";
+import { TYear } from "src/Components/Admin/TYear/TYear";
+import { TContest } from "src/Components/Admin/TContest/TContest";
 
 export const SECTIONS_DASHBOARD = [
   {
@@ -14,8 +18,28 @@ export const SECTIONS_DASHBOARD = [
     Component: TUsers,
   },
   {
+    path: "participants",
+    title: "Participantes",
+    Component: TParticipant,
+  },
+  {
     path: "careers",
     title: "Carreras",
     Component: TCareers,
+  },
+  {
+    path: "typesContests",
+    title: "Tipos de concurso",
+    Component: TTypeContest,
+  },
+  {
+    path: "contests",
+    title: "Concursos",
+    Component: TContest,
+  },
+  {
+    path: "years",
+    title: "Años de carreras",
+    Component: TYear,
   },
 ];
