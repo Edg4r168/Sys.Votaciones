1c4c3a8a32108dec99251fe125bf389cc28fa092
edgar sibrian <edg4r168@gmail.com>
2024-04-03 14:23:42 -0600

    create component FormDelete

---
+++ SysVotaciones.WebApp/UI/src/Components/Admin/TContest/FormDelete.jsx
@@ -0,0 +1,30 @@
+import { ButtonGreen } from "src/Components/Layout/Buttons";
+import { Form } from "../Forms/Form";
+import { FormGroup } from "../Forms/FormGroup";
+import { useTable } from "src/hooks/useTable";
+import { useKeyUp } from "src/hooks/useKeyUp";
+
+export function FormDelete({ onSubmit }) {
+  const { remove, entry } = useTable();
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    remove(entry.studentCode);
+    onSubmit && onSubmit();
+  };
+
+  useKeyUp(handleOnSubmit);
+
+  return (
+    <Form onSubmit={handleOnSubmit}>
+      <FormGroup>
+        <p className="warning-message">
+          ¿Estas seguro de borrar este registro?
+        </p>
+      </FormGroup>
+
+      <ButtonGreen>Aceptar</ButtonGreen>
+    </Form>
+  );
+}
