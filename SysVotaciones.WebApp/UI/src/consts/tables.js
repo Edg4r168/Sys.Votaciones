3ffba12661ee42257a018e5b248a6bca7dea8d10
edgar sibrian <edg4r168@gmail.com>
2024-04-03 14:32:57 -0600

    create const tables

---
+++ SysVotaciones.WebApp/UI/src/consts/tables.js
@@ -0,0 +1,9 @@
+export const TABLES = {
+  categories: "Category",
+  users: "Student",
+  participants: "Participant",
+  careers: "Career",
+  typesContests: "TypeContest",
+  contests: "Contest",
+  years: "Year",
+};
